eaf10beed2fee27a94ca0f15bbf62e73210f68b5
Nuno Nunes <nunonunes@Nunos-iMac.local>
2018-03-03 17:55:04 +0000

    first commit

---
+++ PTNeideSimoes/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  PTNeideSimoes
+//
+//  Created by Nuno Nunes on 27/02/2018.
+//  Copyright © 2018 Nuno Nunes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var emailControlXib: EmailControlXib!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = #colorLiteral(red: 0.9568627451, green: 0.9568627451, blue: 0.9568627451, alpha: 1)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
