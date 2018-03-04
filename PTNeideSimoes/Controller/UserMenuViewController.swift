e2f3bd3c63170e10b1c7137fb7e379d31477d4b8
Nuno Nunes <nunonunes@Nunos-iMac.local>
2018-03-04 23:58:22 +0000

    created Side Sliding VCs

---
+++ PTNeideSimoes/Controller/UserMenuViewController.swift
@@ -0,0 +1,36 @@
+//
+//  UserMenuViewController.swift
+//  PTNeideSimoes
+//
+//  Created by Nuno Nunes on 04/03/2018.
+//  Copyright © 2018 Nuno Nunes. All rights reserved.
+//
+
+import UIKit
+
+class UserMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.8784313725, green: 0.09803921569, blue: 0.1568627451, alpha: 1)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
